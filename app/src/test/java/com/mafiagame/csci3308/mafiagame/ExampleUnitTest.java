264b2b6bffefe61b4d846d74f1432cde04489d57
soorin1993 <soorin1993@gmail.com>
2016-02-22 16:49:17 -0700

    Initial android app. Hello World!

---
+++ app/src/test/java/com/mafiagame/csci3308/mafiagame/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.mafiagame.csci3308.mafiagame;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
