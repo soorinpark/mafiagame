7a1d635b58c169027234f7ddc716e617ce622194
caow7208@colorado.edu <caow7208@colorado.edu>
2016-04-15 10:43:38 -0600

    Added background

--- app/src/main/java/com/mafiagame/csci3308/mafiagame/About.java
+++ app/src/main/java/com/mafiagame/csci3308/mafiagame/About.java
@@ -8,6 +8,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
 
+
 public class About extends Activity {
 
     Button button;
@@ -16,6 +17,8 @@ public class About extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
+
+
     }
 
 }
